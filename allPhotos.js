a02b90a1130b447382f453d495f76f3388ea6706
kasi <kasionaga@gmail.com>
2015-10-21 12:40:44 -1000

    Made allPhotos.js, fixed mixins, made allPhotos into function so pop wouldnt delete each pics

---
+++ allPhotos.js
@@ -0,0 +1,36 @@
+var allPhotos = function() {
+
+  var photos = [
+  { image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
+    title : 'blah blah - ',
+    descritption : 'this is the descritption',
+    copyright : 'THIS IS COPYRIGHT',
+    link : 'https://www.pexels.com/',
+    id : 1
+    },
+  { image : 'https://static.pexels.com/photos/8825/food-wood-coffee-mug.jpg',
+    title : 'blah blah - ',
+    descritption : 'this is the descritption',
+    copyright : 'THIS IS COPYRIGHT',
+    link : 'https://www.pexels.com/',
+    id : 2
+    },
+  { image : 'https://static.pexels.com/photos/9287/nature-water-flowers-purple.jpg',
+    title : 'blah blah - ',
+    descritption : 'this is the descritption',
+    copyright : 'THIS IS COPYRIGHT',
+    link : 'https://www.pexels.com/',
+    id : 3
+  },
+  { image : 'https://static.pexels.com/photos/10913/pexels-photo-10913.jpeg',
+    title : 'blah blah - ',
+    descritption : 'this is the descritption',
+    copyright : 'THIS IS COPYRIGHT',
+    link : 'https://www.pexels.com/',
+    id : 3
+  }
+  ];
+  return photos;
+};
+
+module.exports = allPhotos;
--- router/router.js
+++ router/router.js
@@ -3,34 +3,15 @@ var router = express.Router();
 
 // ================== Home Route ====================
 router.get('/', function (req, res) {
-  res.render('./home-listing', {
-    photos : [{
-      image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-      title : 'blah blah - ',
-      link : 'https://www.pexels.com/',
-      id : 1
-    }, {
-      image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-      title : 'blah blah - ',
-      link : 'https://www.pexels.com/',
-      id : 2
-    }, {
-      image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-      title : 'blah blah - ',
-      link : 'https://www.pexels.com/',
-      id : 3
-    }],
-    mainPhoto : [{
-      image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-      id : 0
-    }]
+  res.render('home-listing', {
+    photos : allPhotos.photos
   });
 });
 
 // =============== New Photo Form Route =====================
 // Just the view for the users to submit a photo
 router.get('/gallery/new', function (req, res) {
-  res.render('./layouts/new-photo', {
+  res.render('new-photo', {
     submitButton : 'Submit Photo'
   });
 });
@@ -43,7 +24,7 @@ router.post('/gallery/new', function (req, res) {
 
 // =============== Edit Gallery Photo by id =================
 router.get('/gallery/:id/edit', function (req, res) {
-  res.render('./layouts/edit-photo', {
+  res.render('edit-photo', {
     editButtonText : 'Edit Photo'
   });
 });
@@ -52,27 +33,10 @@ router.get('/gallery/:id/edit', function (req, res) {
 // If you have multiple HTTP methods for one route
 router.route('/gallery/:id')
   .get(function(req, res) {
-    res.render('./layouts/home-detail', {
-      mainPhoto : [{
-        image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-        title : 'blah blah - ',
-        link : 'https:www.pexels.com/',
-        description : 'Candy canes candy canes apple pie sweet roll powder. Bear claw brownie tootsie roll dragée. Sweet roll croissant brownie candy canes liquorice. Sweet roll oat cake caramels cookie cotton candy. Danish brownie toffee.',
-        copyright : 'Copyright www.pexels.com/'
-      }],
-      photos : [{
-        image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-        title : 'blah blah - ',
-        link : 'https://www.pexels.com/'
-      }, {
-        image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-        title : 'blah blah - ',
-        link : 'https://www.pexels.com/'
-      }, {
-        image : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
-        title : 'blah blah - ',
-        link : 'https://www.pexels.com/'
-      }]
+    var allPhotos = require('./../allPhotos')();
+    res.render('home-detail', {
+      'mainPhoto' : allPhotos.pop(),
+      'photos' : allPhotos
     });
   })
   .delete(function(req, res) {
