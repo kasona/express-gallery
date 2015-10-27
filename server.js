62f6ecb1a29acd26234f575946da0f9a8331094c
kasi <kasionaga@gmail.com>
2015-10-27 09:43:17 -1000

    Added sequelizer

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ var jade = require('jade');
 
 var router = require('./router/router');
 
-// var mongo = require('mongodb');
+var db = require('./models');
+var User = db.User;
+
+var bodyParser = require('body-parser');
 
 // ============== Adding Template Engine ============
 // Tell Express which Template engine we are using by NPM module name
@@ -20,14 +23,35 @@ app.set('views', __dirname + '/views');
 // ======== Hi Express, look in here & process these files =======
 app.use(express.static('./public'));
 
-// =========== Routes ======================
+// ============ Parser ==================
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+// ============ Routes ======================
 app.use('/', router);
+
+
+app.get('/users', function(req, res) {
+  User.findAll()
+  .then(function (users) {
+    res.json(users);
+  });
+});
+
+app.post('/users', function (req, res) {
+  User.create({ username: req.body.username })
+  .then(function (user) {
+    res.json(user);
+  });
+});
+
 
 // Creating a server listening on port 3000
 // listen is express's way of creating a HTTP server
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
+  db.sequelize.sync();
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
