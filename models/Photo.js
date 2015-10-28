9204d64b368bb28cedae64c88a7a94f67f2c5884
kasi <kasionaga@gmail.com>
2015-10-27 18:57:29 -1000

    Added a Photo.js to create db

---
+++ models/Photo.js
@@ -0,0 +1,36 @@
+// Create a Photo table
+module.exports = function(sequelize, DataTypes) {
+  var Photo = sequelize.define( "photo", {
+    url : DataTypes.STRING,
+    title : DataTypes.TEXT,
+    author : DataTypes.TEXT,
+    description : DataTypes.TEXT
+  }, {
+    // Disable modification of tablenames. makes table into photo not photos
+    freezeTableName : true
+  });
+
+
+  Photo.sync({ force : true }).then(function() {
+    Photo.create({
+      url : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
+      title : 'this is title',
+      author : 'banana',
+      descritption : 'this is the descritption'
+    });
+    Photo.create({
+      url : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
+      title : 'this is title',
+      author : 'banana',
+      descritption : 'this is the descritption'
+    });
+    Photo.create({
+      url : 'https://static.pexels.com/photos/1127/cold-snow-landscape-nature.jpg',
+      title : 'this is title',
+      author : 'banana',
+      descritption : 'this is the descritption'
+    });
+  });
+
+  return Photo;
+};
