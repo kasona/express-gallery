6a5e3a28002e8b01b65e0ea22ed67e303d90c027
kasi <kasionaga@gmail.com>
2015-10-28 14:21:12 -1000

    Made /:id findById, need to create the side bar for home-detail

--- routes/gallery.js
+++ routes/gallery.js
@@ -28,7 +28,7 @@ router.post('/', function (req, res) {
       description : req.body.description
     })
   .then(function(newPhoto) {
-    res.redirect('/');
+    res.redirect('/gallery/:' + newPhoto.id);
   });
 });
 
@@ -46,23 +46,37 @@ router.get('/new', function (req, res) {
 /**
  * Gallery Photo Id Route +
  * if you have multiple http mehtods for one route
- * wanna use this?
+ * findById makes the main photo
  */
 
-router.route('/:id')
-  .get(function(req, res) {
-    var allPhotos = require('./../allPhotos')();
-    res.render('home-detail', {
-      'mainPhoto' : allPhotos.pop(),
-      'photos' : allPhotos
-    });
-  })
-  .delete(function(req, res) {
-    res.send('be able to delete this gallery photo, identified by the :id param');
-  })
-  .put(function(req, res) {
-    res.send('be able to edit this gallery photo, identified by the :id param');
-  });
+router.get('/:id', function(req, res) {
+  Photo.findById(req.param.id)
+    .then(function(single) {
+      res.render('home-detail', {
+        mainPhoto : photos.pop(),
+        id : req.params.id
+      })
+    })
+    .then(function(single, multiple) {
+
+    })
+});
+
+
+
+  // .get(function(req, res) {
+  //   var allPhotos = require('./../allPhotos')();
+  //   res.render('home-detail', {
+  //     'mainPhoto' : allPhotos.pop(),
+  //     'photos' : allPhotos
+  //   });
+  // })
+  // .delete(function(req, res) {
+  //   res.send('be able to delete this gallery photo, identified by the :id param');
+  // })
+  // .put(function(req, res) {
+  //   res.send('be able to edit this gallery photo, identified by the :id param');
+  // });
 
 /**
  * Edit Gallery Photo by id
